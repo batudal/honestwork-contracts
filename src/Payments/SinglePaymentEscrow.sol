fdca7eec1e55baebacfadcf917c4efd07d340f75
mertcmutlu <mertcmutlu@gmail.com>
2022-12-13 15:23:10 +0300

    more payment stuff

--- src/Payments/SinglePaymentEscrow.sol
+++ src/Payments/SinglePaymentEscrow.sol
@@ -6,7 +6,10 @@ contract SinglePaymentEscrow {
 
     IERC20 public paymentToken;
     uint256 public totalAmount;
+    uint256 public honestWorkFee;
     address public creator;
+    address public employer;
+    address public HonestWorkFeeCollector;
     Status public status;
     bool public nativePayment;
 
@@ -15,24 +18,49 @@ contract SinglePaymentEscrow {
         DepositedPayment,
         CreatorAccepted,
         JobCompleted,
-        PaymentTransferred
+        PaymentTransferred,
+        JobCancelled
     }
 
 
-    constructor(bool _nativePayment,address _paymentToken, uint256 _totalAmount, address _creator) {
+    constructor(bool _nativePayment,address _paymentToken, uint256 _totalAmount, address _creator, address _employer, uint256 _honestWorkFee) {
         paymentToken = IERC20(_paymentToken);
         totalAmount = _totalAmount;
         creator = _creator;
         status = Status.ContractCreated;
         nativePayment = _nativePayment;
+        employer = _employer;
+        honestWorkFee = _honestWorkFee;
     }
 
-    function deposit() external payable returns(bool) {
+    modifier onlyEmployer() {
+        require(msg.sender == employer, "Not owner");
+        _;
+    }
+
+    function deposit() external payable onlyEmployer returns(bool) {
         if(nativePayment) {
             require(msg.value > totalAmount, "you must deposit the entire payment");
             status = Status.DepositedPayment;
+            return true;
         } else {
-            paymentToken.transferFrom(msg.sender, address(this), totalAmount);
+            paymentToken.transferFrom(msg.sender, address(this), totalAmount * (100 - honestWorkFee) / 100);
+            paymentToken.transferFrom(msg.sender, HonestWorkFeeCollector, totalAmount * honestWorkFee / 100);
+            status = Status.DepositedPayment;
+            return true;
+        }
+    }
+
+    function cancelJob() external onlyEmployer returns(bool) {
+        require(status == Status.DepositedPayment || status == Status.CreatorAccepted);
+            if(nativePayment) {
+            (bool sent, ) = employer.call{value: totalAmount * (100 - honestWorkFee) / 100}("");
+            require(sent, "Failed to send payment");
+            return true;
+        } else {
+            paymentToken.transferFrom(address(this), employer, totalAmount * (100 - honestWorkFee) / 100);
+            status = Status.DepositedPayment;
+            return true;
         }
     }
 
